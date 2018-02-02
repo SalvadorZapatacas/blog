e20ad17ab78f42c62a18820ed5668460baf0d217
Salvador <salvazapata@outlook.com>
2018-02-02 17:36:03 +0100

    Rediseño página principal

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,8 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //Utilizamos latest , funcion de Eloquent
+    $posts = App\Post::latest('published_at')->get();
+    return view('welcome',compact('posts'));
 });
+
