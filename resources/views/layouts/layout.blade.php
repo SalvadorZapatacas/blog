a646effb55ff9923fa65ff98662b79cf001ae145
Salvador <salvazapata@outlook.com>
2018-02-02 19:21:27 +0100

    Tag Model , migrations de Tags y tabla pivote, cambio interfaz

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -31,13 +31,6 @@
 
 @yield('content')
 
-<div class="pagination">
-    <ul class="list-unstyled container-flex space-center">
-        <li><a href="#" class="pagination-active">1</a></li>
-        <li><a href="#">2</a></li>
-        <li><a href="#">3</a></li>
-    </ul>
-</div>
 
 <section class="footer">
     <footer>
