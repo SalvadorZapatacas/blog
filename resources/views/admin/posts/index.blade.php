c32d9bf237a6d77bb9b8d5f25a7218ec73610595
Salvador <salvazapata@outlook.com>
2018-02-06 16:23:53 +0100

    Se mejora la vista de admin

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -1,1 +1,22 @@
-Mostrar todos los post
+@extends('admin.layouts.layout')
+
+@section('header')
+
+    <h1>
+        Todas las publicaciones
+        <small>Listado completo de Posts</small>
+    </h1>
+    <ol class="breadcrumb">
+        <li><a href="{{ route('admin.home') }}"><i class="fa fa-dashboard"></i> Inicio</a></li>
+        <li class="active">Posts</li>
+    </ol>
+
+@endsection
+
+
+
+
+@section('content')
+
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,15 @@ Route::get('/', 'PagesController@home');
 
 Route::get('/home' , 'HomeController@index')->name('admin.home');
 
-Route::get('admin/posts' , 'Admin\PostsController@index')->name('admin.posts.index');
+
+Route::group([
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'middleware' => 'auth'],
+    function() {
+        Route::get('posts' , 'PostsController@index')->name('admin.posts.index');
+        //Resto de rutas de administración
+});
 
 // Authentication Routes...
 Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
