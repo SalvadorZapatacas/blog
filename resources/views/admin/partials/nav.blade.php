9319415cb65d2425d0d195475ac191bf9a016a95
Salvador <salvazapata@outlook.com>
2018-02-07 15:43:30 +0100

    Pequeño retoques de diseño

--- resources/views/admin/partials/nav.blade.php
+++ resources/views/admin/partials/nav.blade.php
@@ -2,16 +2,16 @@
 <ul class="sidebar-menu" data-widget="tree">
     <li class="header">@lang('admin.layout.header')</li>
     <!-- Optionally, you can add icons to the links -->
-    <li class="active"><a href="{{ route('admin.dashboard') }}"><i class="fa fa-dashboard"></i> <span>Inicio</span></a></li>
+    <li {{ request()->is('admin') ? 'class=active' : ''  }}><a href="{{ route('admin.dashboard') }}"><i class="fa fa-dashboard"></i> <span>Inicio</span></a></li>
     <!--<li><a href="#"><i class="fa fa-link"></i> <span>Another Link</span></a></li>-->
-    <li class="treeview">
+    <li class="treeview {{ request()->is('admin/posts') ? 'active' : ''  }}">
         <a href="#"><i class="fa fa-link"></i> <span>Blog</span>
             <span class="pull-right-container">
                 <i class="fa fa-angle-left pull-right"></i>
               </span>
         </a>
         <ul class="treeview-menu">
-            <li><a href="{{ route('admin.posts.index') }}"><i class="fa fa-eye"></i><span>Ver todos los post</span></a></li>
+            <li {{ request()->is('admin/posts') ? 'class=active' : ''  }}><a href="{{ route('admin.posts.index') }}"><i class="fa fa-eye"></i><span>Ver todos los post</span></a></li>
             <li><a href="#"><i class="fa fa-pencil"></i><span>Crear un nuevo post</span></a></li>
         </ul>
     </li>
